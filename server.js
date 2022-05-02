100de9c65dd00afd60b5e0b8f0815fdb7dbb6ee8
ishelly1997 <ishelly1997@gmail.com>
2022-05-01 21:56:57 -0400

    deplo

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
-
+app.use(express.static("public"));
 app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
